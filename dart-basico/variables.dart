dedb94c4e688353deb10677105c09dd79d43aa2a
respinoza-arch <r.espinoza@eston.ecotec.edu.ec>
2026-07-23 23:56:43 -0500

    Prueba

---
+++ dart-basico/variables.dart
@@ -0,0 +1,21 @@
+void main() {
+  //for (var i = 0; i < 10; i++) {
+   // print('hello ${i + 1}');
+  // }
+  final String nombre2 = "Fabian";
+  
+  // TAREA: Declare un const y que sucede si cambia el valor
+  
+  //var nombre = 'Richard';
+  //var num1 = '10';
+  //var num2 = 20;
+  
+  String nombre = 'Richard';
+  int num1 = 10;
+  int num2 = 20;
+  String num3 = '30';
+ 
+  print('${num1 + num2 + num3}');
+  
+  print('Mi nombre es $nombre tengo ${num1 + num2}');
+}
