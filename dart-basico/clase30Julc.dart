237d2aa2ba91178428e0dba6812467380c353fb2
respinoza-arch <r.espinoza@eston.ecotec.edu.ec>
2026-07-30 23:15:07 -0500

    Maps

---
+++ dart-basico/clase30Julc.dart
@@ -0,0 +1,24 @@
+void main() {
+
+  // Mapas, diccionarios, key value
+  
+  Map persona = {
+    'nombre': 'Richard',
+    'apellido': 'Espinoza',
+    'activo': true,
+    1: 10000,
+    0.4: 200
+  };
+  
+  print(persona); // Imprime todo el Map (llave y valor)
+  // Imprime: {nombre: Richard, apellido: Espinoza, activo: true, 1: 10000, 0.4: 200}
+    
+  print(persona['apellido']); // Imprime el valor de la llave 'apellido'
+  
+  // Las variables se declaran usando camelCase(variables, funciones y métodos)
+  
+  // Donde no se usa el camelCase: en clases, en archivos
+  
+ // String nombreCompleto;
+  
+}
