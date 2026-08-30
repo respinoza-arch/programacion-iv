2d2a2c325a274569e86a945e3c9f114f7d569fbe
respinoza-arch <r.espinoza@eston.ecotec.edu.ec>
2026-08-30 13:52:21 -0500

    Empezando a actualizar el proyecto integrador 1 para usarlo en el integrador 2

---
+++ mi_pasatiempo/mi_pasatiempo/lib/pantallas/nudos_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NudosScreen extends StatelessWidget {
+   
+const NudosScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+         child: Text('NudosScreen'),
+      ),
+    );
+  }
+}
--- mi_pasatiempo/mi_pasatiempo/test/widget_test.dart
+++ mi_pasatiempo/mi_pasatiempo/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mi_pasatiempo/main.dart';
+import 'package:mi_pasatiempo/main_old2.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
